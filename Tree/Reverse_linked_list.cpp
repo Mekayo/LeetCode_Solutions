142d9c9880ba601921f54957f0d659a2e2132e33
Hitesh <hitesh29004@gmail.com>
2025-10-10 01:58:20 +0530

    Add LeetCode #92 - Reverse Linked List

---
+++ Tree/Reverse_linked_list.cpp
@@ -0,0 +1,33 @@
+// LeetCode #92: Reverse Linked List
+// https://leetcode.com/problems/reverse-linked-list
+
+class ListNode {
+public:
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(!head) return head;
+
+        ListNode* ele_at=new ListNode(0);
+        ele_at->next=head;
+        ListNode* prev =ele_at;
+
+        // reach to the left
+        for(int i=0;i<left-1;i++){
+            prev=prev->next;
+        }
+        ListNode* cur=prev->next;
+
+        for(int j=0;j<right-left;j++){
+            ListNode* temp=cur->next;
+            cur->next=temp->next;
+            temp->next=prev->next;
+            prev->next=temp;
+        }
+        return ele_at->next;
+    }
+};
