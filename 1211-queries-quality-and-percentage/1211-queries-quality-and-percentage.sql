81973bbe2891b119d57c327f96a7a42798611e8f
Hitesh sharma <157316486+Mekayo@users.noreply.github.com>
2026-07-09 01:10:20 +0530

    Time: 330 ms (99.78%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,11 +1,5 @@
-# Write your MySQL query statement below
-WITH CTE AS
-(SELECT query_name,rating/position AS ratio,
-    CASE WHEN rating < 3 THEN 1
-    ELSE 0 END AS quality_ratio
-FROM Queries)
-
-SELECT query_name, ROUND(AVG(ratio),2) AS quality,
-    ROUND(SUM(quality_ratio)/COUNT(*)*100,2) AS poor_query_percentage
-FROM CTE
-GROUP BY query_name;
+SELECT query_name,
+        ROUND(AVG(rating / position), 2) as quality,
+        ROUND(SUM(rating <3) / count(*) * 100, 2) as poor_query_percentage
+FROM Queries
+GROUP BY query_name
